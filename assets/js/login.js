1a636ce2d5c29e04a95843953840d89529acc5e6
codeboy <1010232788@qq.com>
2022-05-18 16:29:33 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,70 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
+$(function(){
+  $('#link_reg').on('click',()=>{
     $('.login-box').hide()
     $('.reg-box').show()
   })
-
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
+  $('#link_login').on('click',()=>{
     $('.login-box').show()
     $('.reg-box').hide()
   })
+ // 通过layui获取form对象
+ let form =layui.form
+//  获取layer对象
+let layer =layui.layer
+ // 获取form的verify方法自定义校验规则
+ form.verify({
+  //  自定义了一个叫pwd的校验规则
+   pwd:[
+     /^[\S]{6,12}$/
+     ,'密码必须6到12位，且不能出现空格'
+   ] 
+   ,repwd:function(value){
+    //  通过形参获取重复密码的value
+    let repwd =value
+    // 在获取密码的value
+    let pwd=$('.reg-box [name=password]').val()
+    // 然后进行依次判断，看这两个的值是否完全相等
+    if(pwd !== repwd) return '两次输入密码不一致'
+    // 如果不相等，就return一个错误信息
+   }
+ })
 
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
-  form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
+//  注册表单的提交
+//  监听提交事件
+ $('#form-reg').on('submit',function(e){
+  //  阻止form表单的默认提交时间
+  e.preventDefault()
+  // 发起post请求
+  $.post('/api/reguser',
+  {
+    username:$('#form-reg [name=username]').val(),password:$('#form-reg [name=password]').val()
+  },function(res){
+    if(res.status!==0){
+      return layer.msg(res.message)
     }
+    layer.msg('注册成功')
+    $('#link_login').click()
   })
+ })
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
+//  登录表单的提交
+// 监听提交时间
+$('#form-login').on('submit',function(e){
+  // 组织form表单的默认推荐行为
+  e.preventDefault()
+  // 发起登录请求
+  $.post('/api/login',{
+    username:$('#form-login [name=username]').val(),password:$('#form-login [name=password]').val()
+  },function(res){
+    if(res.status!==0){
+      return layer.msg(res.message)
     }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
-
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
-        }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-      }
-    })
+    layer.msg('登陆成功')
+    // 登陆成功后得到的字符串，保存到locakStorage中
+    localStorage.setItem('token',res.token)
+    // 跳转到index主页
+    location.href='/index.html'
   })
 })
+})
+ 
